e44d59ba25c4fd8b74bf7190db6b458f481b9c95
pluselc <user@user-no-MacBook-Pro.local>
2017-03-12 02:27:45 +0900

    implement user list

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,10 +1,14 @@
 package controllers
 
+
 import javax.inject.Inject
 
+import models.Tables._
 import play.api.db.slick._
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
+import slick.jdbc.H2Profile.api._
 import slick.jdbc.JdbcProfile
 
 
@@ -12,7 +16,11 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
                                val messageApi: MessagesApi) extends Controller
   with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
 
-  def list = TODO
+  def list = Action.async { implicit rs =>
+    db.run(Users.sortBy(t => t.id).result).map {
+      users => Ok(views.html.user.list(users))
+    }
+  }
 
   def edit(id: Option[Long]) = TODO
 
