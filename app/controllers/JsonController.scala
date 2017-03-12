123b13c14a09799f1e256fd30df450d89a78950d
pluselc <user@user-no-MacBook-Pro.local>
2017-03-12 20:50:34 +0900

    implement update api

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -11,13 +11,26 @@ import play.api.mvc._
 import slick.jdbc.H2Profile.api._
 import slick.jdbc.JdbcProfile
 
+import scala.concurrent.Future
+
 
 object JsonController {
+
+  private val AUTO_INCREMENT_COLUMN_DUMMY_VALUE = 0
   implicit val UsersRowWritesWrites = (
     (__ \ "id").write[Long] and
       (__ \ "name").write[String] and
       (__ \ "companyId").writeNullable[Int]
     ) (unlift(UsersRow.unapply))
+
+  implicit val UserFormFormat = (
+    (__ \ "id").readNullable[Long] and
+      (__ \ "name").read[String] and
+      (__ \ "companyId").readNullable[Int]
+    ) (UserForm)
+
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
 }
 
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider)
@@ -31,9 +44,31 @@ class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider)
     }
   }
 
-  def create = TODO
+  def create = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      val user = UsersRow(AUTO_INCREMENT_COLUMN_DUMMY_VALUE, form.name, form.companyId)
+      db.run(Users += user).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      Future {
+        BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
-  def update = TODO
+  def update = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      val user = UsersRow(form.id.get, form.name, form.companyId)
+      db.run(Users.filter(t => t.id === user.id.bind).update(user)).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      Future {
+        BadRequest(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
   def remove(id: Long) = TODO
 }
