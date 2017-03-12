d8e66fae5195f26651eb67b0776a734f33240444
pluselc <user@user-no-MacBook-Pro.local>
2017-03-12 19:09:09 +0900

    implement_user_form

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -4,6 +4,8 @@ package controllers
 import javax.inject.Inject
 
 import models.Tables._
+import play.api.data.Forms._
+import play.api.data._
 import play.api.db.slick._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -11,10 +13,26 @@ import play.api.mvc._
 import slick.jdbc.H2Profile.api._
 import slick.jdbc.JdbcProfile
 
+import scala.concurrent.Future
+
+object UserController {
+
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  val userForm = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "name" -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
 
 class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
-                               val messageApi: MessagesApi) extends Controller
-  with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+                               val messagesApi: MessagesApi)
+  extends Controller with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
+
+  import UserController._
 
   def list = Action.async { implicit rs =>
     db.run(Users.sortBy(t => t.id).result).map {
@@ -22,7 +40,23 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
     }
   }
 
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    val form = if (id.isDefined) {
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        UserController.userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      Future {
+        userForm
+      }
+    }
+
+    form.flatMap { form =>
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.user.edit(form, companies))
+      }
+    }
+  }
 
   def create = TODO
 
@@ -30,5 +64,4 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
 
   def remove(id: Long) = TODO
 
-  override def messagesApi: MessagesApi = ???
 }
